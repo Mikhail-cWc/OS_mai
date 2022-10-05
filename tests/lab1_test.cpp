f6230463e8fbead759378668237393c9b6cb13c9
Nucyc <misha-kopylov@bk.ru>
2022-10-05 18:51:57 +0300

    added tests and cmake files

--- tests/lab1_test.cpp
+++ tests/lab1_test.cpp
@@ -1,68 +1,94 @@
 #include <gtest/gtest.h>
 
 #include <array>
-#include <filesystem>
 #include <fstream>
+#include <cstdio>
 #include <memory>
+//#include <filesystem>
+// namespace fs = std::filesystem;
 
-namespace fs = std::filesystem;
-
-extern "C" {
-    #include <parent.h>
+extern "C++"
+{
+#include <utils.h>
 }
 
-TEST(FirstLabTests, SimpleTest) {
-    const char* fileWithInput = "input.txt";
-    const char* fileWithOutput = "output.txt";
+TEST(FirstLabTests, SimpleTest)
+{
+    const char *fileWithInput = "C:\\project\\OS_mai\\build\\tests\\input.txt";
+    const char *fileWithOutput1 = "C:\\project\\OS_mai\\build\\tests\\output1.txt";
+    const char *fileWithOutput2 = "C:\\project\\OS_mai\\build\\tests\\output2.txt";
 
-    constexpr int inputSize = 4;
+    constexpr int inputSize = 6;
+    constexpr int outputSize = 3;
 
-    std::array<const char*, inputSize> input = {
-            "1 2 3 4",
-            "0 3 2",
-            "-10 -10 -10",
-            "1337"
-    };
+    std::array<const char *, inputSize> input = {
+        "1234",
+        "032",
+        "12345",
+        "123456",
+        "987654321",
+        "9463"};
 
-    std::array<int, inputSize> expectedOutput = {
-            10, 5, -30, 1337
-    };
+    std::array<int, outputSize> expectedOutput1 = {
+        123456789, 54321, 230};
+
+    std::array<int, outputSize> expectedOutput2 = {
+        3649, 654321, 4321};
 
     {
-        auto inFile = std::ofstream(fileWithInput);
+        std::ofstream inFile(fileWithInput);
 
-        inFile << fileWithOutput << '\n';
+        //    inFile << fileWithOutput << '\n';
 
-        for(const auto& line : input) {
-            inFile<< line << '\n';
+        for (const auto &line : input)
+        {
+            inFile << line << '\n';
         }
     }
 
-    auto deleter = [](FILE* file) {
+    auto deleter = [](FILE *file)
+    {
         fclose(file);
     };
 
     std::unique_ptr<FILE, decltype(deleter)> inFile(fopen(fileWithInput, "r"), deleter);
 
-    ParentRoutine(getenv("PATH_TO_CHILD"), inFile.get());
+    ParentRoutine(fileWithInput, fileWithOutput1, fileWithOutput2);
 
-    auto outFile = std::ifstream(fileWithOutput);
+    std::ifstream outFile1(fileWithOutput1);
 
-    ASSERT_TRUE(outFile.good());
+    ASSERT_TRUE(outFile1.good());
+
+    std::ifstream outFile2(fileWithOutput2);
+
+    ASSERT_TRUE(outFile2.good());
 
     int result;
 
-    for(int i : expectedOutput) {
-        outFile >> result;
+    for (int i : expectedOutput1)
+    {
+        outFile1 >> result;
         EXPECT_EQ(result, i);
     }
 
-    auto removeIfExists = [](const char* path) {
-        if(fs::exists(path)) {
-            fs::remove(path);
-        }
-    };
+    result = -1;
 
-    removeIfExists(fileWithInput);
-    removeIfExists(fileWithOutput);
+    for (int i : expectedOutput2)
+    {
+        outFile2 >> result;
+        EXPECT_EQ(result, i);
+    }
+    /*
+
+       auto removeIfExists = [](const char *path)
+        {
+            if (fs::exists(path))
+            {
+                fs::remove(path);
+            }
+        };
+
+        removeIfExists(fileWithInput);
+        removeIfExists(fileWithOutput1);
+        removeIfExists(fileWithOutput2); */
 }
